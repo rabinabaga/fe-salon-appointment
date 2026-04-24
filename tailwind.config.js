b2eea45b62b452de4b17cc3b17f442113db44de0
rabinabaga <rabinabaga@gmail.com>
2026-04-24 16:16:25 +0545

    fix css with custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // important
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        espresso: {
+          DEFAULT: '#2c1810',
+          light:   '#4a2c20',
+        },
+        cream: {
+          50:  '#fdfaf5',
+          100: '#f5ede0',
+          200: '#e8d5b7',
+        },
+        gold: {
+          DEFAULT: '#c9a84c',
+          dark:    '#a8893a',
+        },
+      },
+    },
   },
   plugins: [],
 }
